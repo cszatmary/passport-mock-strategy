fb721a53e027c4c181c8d8652b6dea681233014d
cszatma <christopherszatmary@gmail.com>
2018-04-11 00:35:23 -0400

    Added mock-user

---
+++ src/mock-user.js
@@ -0,0 +1,9 @@
+const mockUser = {
+    id: '1234',
+    displayName: 'Foo Bar',
+    name: { familyName: 'Bar', givenName: 'Foo' },
+    emails: [{ value: 'foo@bar.com', type: 'account' }],
+    provider: 'mock',
+};
+
+module.exports = mockUser;
