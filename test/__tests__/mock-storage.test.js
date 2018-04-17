a246dd91432a11bc390843b424e8c9eb446b423c
cszatma <christopherszatmary@gmail.com>
2018-04-17 19:33:52 -0400

    Added tests for mock-storage

---
+++ test/__tests__/mock-storage.test.js
@@ -0,0 +1,15 @@
+const mockStorage = require('../../src/mock-storage')();
+
+describe('Mock storage tests', () => {
+    it('should add a user to the storage and return a promise that resolves the user', () => {
+        const user = { id: '1234', name: 'Foo' };
+        return expect(mockStorage.saveUser(user)).resolves.toEqual(user);
+    });
+
+    it('should return the user with a matching id', () => {
+        return expect(mockStorage.fetchUser('1234')).resolves.toEqual({
+            id: '1234',
+            name: 'Foo',
+        });
+    });
+});
