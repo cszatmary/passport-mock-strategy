38a64edac9ef34677cda09110baaeebf766dd4f2
cszatma <christopherszatmary@gmail.com>
2018-04-17 19:44:54 -0400

    Added tests for using a custom callback with MockStrategy

---
+++ test/__tests__/custom-callback-mock.test.js
@@ -0,0 +1,56 @@
+const Agent = require('jest-supertest-cookie-fix');
+const Passport = require('passport').Passport;
+
+const createApp = require('../app');
+const { MockStrategy } = require('../../src');
+const { setupDeserializeAndSerialize } = require('../../src/mock-utilities');
+const createStorage = require('../../src/mock-storage');
+
+const storage = createStorage();
+const passport = new Passport();
+
+setupDeserializeAndSerialize(passport, (id, done) =>
+    storage.fetchUser(id).then(user => done(null, user))
+);
+
+passport.use(
+    new MockStrategy({ name: 'custom-callback-mock' }, async (user, done) => {
+        const existingUser = await storage.fetchUser({ id: user.id });
+
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+
+        const newUser = await storage.saveUser({ id: user.id });
+        done(null, newUser);
+    })
+);
+
+describe('Tests for customized mock strategy', () => {
+    const app = createApp(passport, 'custom-callback-mock');
+    let agent;
+
+    beforeEach(() => {
+        agent = Agent(app);
+    });
+
+    it('should authenticate the user', () => {
+        return agent.get('/auth/mock').expect(200, { status: 'ok' });
+    });
+
+    describe('Test authenticated rotues', () => {
+        beforeEach(() => {
+            return agent.get('/auth/mock');
+        });
+
+        it('should return the current user', () => {
+            return agent.get('/current-user').expect(200, { id: '1234' });
+        });
+
+        it('should logout the user', () => {
+            return agent.get('/logout').then(() => {
+                agent.get('/current-user').expect(200, {});
+            });
+        });
+    });
+});
