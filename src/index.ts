579635af765fe75a667cc8717f739c772c89fd9d
cszatma <christopherszatmary@gmail.com>
2018-05-05 19:59:32 -0400

    Fixed type of library export

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
 import createMockPassport = require('./create-mock-passport');
 import createMockStorage = require('./mock-storage');
 import mockUser = require('./mock-user');
+import { User } from './mock-user';
 import {
     connectPassport,
     setupSerializeAndDeserialize,
 } from './mock-utilities';
-import MockStrategy = require('./passport-mock-strategy');
+import Strategy = require('./passport-mock-strategy');
 
 /**
  * Export MockStrategy.
  */
-const exportModule: any = MockStrategy;
-exportModule.MockStrategy = MockStrategy;
-exportModule.Strategy = MockStrategy;
+declare namespace MockStrategy {
+    export type Export = typeof Strategy & {
+        MockStrategy: typeof Strategy;
+        Strategy: typeof Strategy;
+        mockUser: User;
+        setupSerializeAndDeserialize: typeof setupSerializeAndDeserialize;
+        connectPassport: typeof connectPassport;
+        createMockPassport: typeof createMockPassport;
+        createMockStorage: typeof createMockStorage;
+    };
+}
+
+const exportModule: any = Strategy;
+exportModule.MockStrategy = Strategy;
+exportModule.Strategy = Strategy;
 exportModule.mockUser = mockUser;
 exportModule.setupSerializeAndDeserialize = setupSerializeAndDeserialize;
 exportModule.connectPassport = connectPassport;
 exportModule.createMockPassport = createMockPassport;
 exportModule.createMockStorage = createMockStorage;
-export = exportModule;
+export = exportModule as MockStrategy.Export;
