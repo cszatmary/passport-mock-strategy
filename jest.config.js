82541298643869696df3ebeb804b20001e7ed770
Christopher Szatmary <cs@christopherszatmary.com>
2019-05-13 23:10:35 -0400

    Migrated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     globals: {
         'ts-jest': {
-            tsConfigFile: 'tsconfig.json',
+            tsConfig: 'tsconfig.json',
         },
     },
     moduleFileExtensions: ['ts', 'js'],
     transform: {
-        '^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js',
+        '^.+\\.(ts|tsx)$': 'ts-jest',
     },
     testMatch: ['**/__tests__/**/*.test.(ts|js)'],
     testEnvironment: 'node',
