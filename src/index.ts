c6e4f480a6f94fd19d43ef8b4c59476fbb6bdb46
Christopher Szatmary <cs@christopherszatmary.com>
2019-05-15 21:55:36 -0400

    Exported additional types

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,19 @@ import MockStrategy from './passport-mock-strategy';
  * Export everything.
  */
 export { default as createMockPassport } from './create-mock-passport';
-export { default as createMockStorage } from './mock-storage';
+export { default as createMockStorage, MockStorage } from './mock-storage';
 export { default as mockUser, User } from './mock-user';
 export {
   connectPassport,
   setupSerializeAndDeserialize,
+  SerializeFn,
+  DeserializeFn,
 } from './mock-utilities';
+export {
+  MockStrategyOptions,
+  DoneCallback,
+  VerifyFunction,
+} from './passport-mock-strategy';
 export { MockStrategy, MockStrategy as Strategy };
 export default MockStrategy;
 
